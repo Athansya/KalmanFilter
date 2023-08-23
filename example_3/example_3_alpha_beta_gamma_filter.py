9d26bebbc1f031a423866857d9c0d2b89ac6b911
Athansya <atoriz98@outlook.com>
2023-08-23 12:03:42 -0600

    Fixed plots, predicted variables where wrongly used

--- example_3/example_3_alpha_beta_gamma_filter.py
+++ example_3/example_3_alpha_beta_gamma_filter.py
@@ -1,28 +1,25 @@
-"""
-File: example_2_alpha_beta_state_extrapolation_equation.py
-Project: Kalman Filter 
-File Created: Tuesday, 15th August 2023 11:22:09 pm
+'''
+File: example_3_alpha_beta_gamma_filter.py
+Project: example_3
+File Created: Tuesday, 22nd August 2023 10:03:25 pm
 Author: Alfonso Toriz Vazquez (atoriz98@comunidad.unam.mx)
 -----
-Last Modified: Tuesday, 15th August 2023 11:45:27 pm
+Last Modified: Tuesday, 22nd August 2023 10:03:37 pm
 Modified By: Alfonso Toriz Vazquez (atoriz98@comunidad.unam.mx>)
 -----
 License: MIT License
 -----
-Description: Estimates an aircraft position and velocity using the state
-extrapolation equations and the alpha beta filters. Based on the example
-2 from Alex Becker's book, Kalman Filter from the Ground up.
-"""
+Description: 
+'''
+#TODO - CHECK EQUATIONS AND FUNCTIONS CALLS, THERE IS AN ERROR ON THE GRAPHS
 
 # ---------------------------------------------------------------------------- #
 # NOTE                          PROBLEM DESCRIPTION                            #
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-#        Imagine an aircraft flying at a constant altitude and velocity.       #
-#    We want to estimate its position from a radar as well as its velocity.    #
-#    For the sake of the example the velocity is obtained from the derivate    #
-#    of the position. The radar tracks the position of the aircraft every 5    #
-#    seconds, known as delta t.                                                #
+#      Now we will track an aircraft that moves at a constant velocity of      #
+#      50 m/s for 20 sec. Then it accelerates with a constant 8 m/s^2 for      #
+#                                  35 seconds.                                 #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 #                                   LIBRARIES                                  #
@@ -38,41 +35,47 @@ import pandas as pd
 # ---------------------------------------------------------------------------- #
 rc("font", **{"family": "serif", "serif": ["Computer Modern"], "size": 16})
 rc("text", usetex=True)
+
 # ---------------------------------------------------------------------------- #
 #                                   CONSTANTS                                  #
 # ---------------------------------------------------------------------------- #
-
 DELTA_T = 5  # Seconds, track-to-track period
-INITIAL_ESTIMATED_POSITION = 30_000  # Meters
-INITIAL_ESTIMATED_VELOCITY = 40  # m/s
+INITIAL_ESTIMATED_POSITION = 30_000  # meters
+INITIAL_ESTIMATED_VELOCITY = 50  # m/s
+INITIAL_ESTIMATED_ACCELERATION = 0  # m/s^2
 MEASUREMENTS = [
-    30171,
-    30353,
-    30756,
-    30799,
-    31018,
-    31278,
-    31276,
-    31379,
-    31748,
-    32175
+    30221,
+    30453,
+    30906,
+    30999,
+    31368,
+    31978,
+    32526,
+    33379,
+    34698,
+    36275
 ]  # Sensor measurements
-ALPHA = 0.2  # Filter param
-BETA = 0.1  # Filter param
-
+ALPHA = 0.5  # Filter param
+BETA = 0.4  # Filter param
+GAMMA = 0.1
 
 # ---------------------------------------------------------------------------- #
 #                         STATE EXTRAPOLATION EQUATION                         #
 # ---------------------------------------------------------------------------- #
 #                                                                              #
-#                Position -> x_{n+1} = x_n + \DeLta t \dot{x}_n                #
-#                Velocity -> \dot{x}_{n+1} = \dot{x}_n  (Constant)             #
+#              Position -> x_{n+1} = x_n + \dot{x}_n Delta t + ...             #
+#                     ... + \ddot{x}_n \frac{\Delta t^2}{2}                    #
+#                                                                              #
+#          Velocity -> \dot{x}_{n+1} = \dot{x}_n + \ddot{x}_n \Delta t         #
+#                                                                              #
+#            Acceleration -> \ddot{x}_{n+1} = \ddot{x}_n (Constant)            #
 #                                                                              #
 # ---------------------------------------------------------------------------- #
 def state_extrapolation_first_equation(
     current_state: float,
     delta: float,
-    current_state_derivative: float
+    current_state_derivative: float,
+    current_state_second_derivative: float
 ) -> float:
     """Obtains predicted state based on current state. Uses the initial
     conditions to extrapolate the value of the first iteration of the
@@ -83,36 +86,57 @@ def state_extrapolation_first_equation(
         delta (float): track-to-track period in seconds $\Delta t$.
         current_state_derivative (float): current state derivative based on
         initial conditions.
+        current_state_second_derivative (float): current state second
+        derivative based on initial conditions
 
     Returns:
         predicted_state (float): predicted state extrapolated from initial
         conditions.
     """
-    predicted_state = current_state + delta * current_state_derivative
+    predicted_state = current_state + current_state_derivative * delta + (
+        current_state_second_derivative * (delta**2) / 2)
     return predicted_state 
 
-def state_extrapolation_second_equation(current_state_derivative: float) -> float:
-    """ 
+def state_extrapolation_second_equation(
+    current_state_derivative: float,
+    current_state_second_derivative: float,
+    delta: float
+) -> float:
+    """Obtains predicted state based on the current state.
 
     Args:
         current_state_derivative (float): current state derivate based on
         initial conditions.
+        current_state_second_derivative (float): current state second
+        derivative based on initial conditions
 
     Returns:
         predicted_state (float): predicted state extrapolated from initial
         conditions.
     """
-    predicted_state_derivate = current_state_derivative  # Constant!!!
+    predicted_state_derivate = current_state_derivative + (
+        current_state_second_derivative * delta)
     return predicted_state_derivate
-# ---------------------------------------------------------------------------- #
-#                              ALPHA - BETA FILTER                             #
-# ---------------------------------------------------------------------------- #
-#                                                                              #
-#    Filter that assumes that in system with two internal states the second    #
-#    state is obtained by the derivate of the first one. A great example is    #
-#     using position's derivative to compute velocity. Its values depend on    #
-#                      the precision of the measurements.                      #
-#                                                                              #
+
+
+def state_extrapolation_third_equation(
+    current_state_second_derivative: float
+) -> float:
+    """Obtains predicted state based on the current state.
+
+    Args:
+        current_state_second_derivative (float): current state second
+        derivative based on initial conditions.
+
+    Returns:
+        predicted_state_second_derivative (float): predicted state
+        extrapolated from current one. 
+    """
+    # Constant!
+    predicted_state_second_derivative = current_state_second_derivative  
+    return predicted_state_second_derivative
+
+
 # ---------------------------------------------------------------------------- #
 #                            STATE UPDATE EQUATIONS                            #
 # ---------------------------------------------------------------------------- #
@@ -123,7 +147,35 @@ def state_extrapolation_second_equation(current_state_derivative: float) -> floa
 #        Velocity -> \hat{\dot{x}}_{n, n} = \hat{\dot{x}}_{n, n-1} + ...       #
 #            ... + \beta (\frac{z_n  - \hat{x}_{n, n-1}}{\Delta t})            #
 #                                                                              #
+#    Acceleration -> \hat{\ddot{x}}_{n, n} = \hat{\ddot{x}}_{n , n-1} + ...    #
+#         ... + \gamma (\frac{z_n - \hat{x}_{n, n-1}}{0.5 \Delta t^2})         #
+#                                                                              #
 # ---------------------------------------------------------------------------- #
+def state_update_first_equation(
+    current_state_prediction: float,
+    alpha: float,
+    current_measurement: float,
+) -> float:
+    """Obtains the current estimate value based on the state update
+    equations and an alpha-beta filter. The alpha value depends on the sensor
+    precision. This equation is obtained by integrating the second one.
+
+    Args:
+        current_state_prediction (float): predicted value of the current 
+        state $x_{n, n}$ based on the previous information $x_{n, n-1}$.
+        beta (float): alpha-beta filter argument.
+        current_measurement (float): current sensor measurement $z_n$.
+
+    Returns:
+        (float): current state estimation based on past and 
+        present information.
+    """
+    current_state_estimate = current_state_prediction + alpha * (
+        current_measurement - current_state_prediction
+    )
+    return current_state_estimate 
+
+
 def state_update_second_equation(
     current_state_prediction_derivative: float,
     beta: float,
@@ -155,67 +207,86 @@ def state_update_second_equation(
     return current_state_estimate_derivative
 
 
-def state_update_first_equation(
+def state_update_third_equation(
+    current_state_prediction_second_derivative: float,
+    gamma: float,
     current_state_prediction: float,
-    alpha: float,
     current_measurement: float,
+    delta: float
 ) -> float:
-    """Obtains the current estimate value based on the state update
-    equations and an alpha-beta filter. The alpha value depends on the sensor
-    precision. This equation is obtained by integrating the second one.
+    """_summary_
 
     Args:
-        current_state_prediction (float): predicted value of the current 
-        state $x_{n, n}$ based on the previous information $x_{n, n-1}$.
-        beta (float): alpha-beta filter argument.
+        current_state_prediction_second_derivative (float): predicted value of 
+        the current state second derivative $\ddot{x}_{n, n}$ based on the previous
+        information $\ddot{x}_{n, n-1}$.
+        gamma (float): alpha-beta-gamma filter argument.
+        current_state_prediction (float): predicted value of the current state
+        $x_{n, n}$ based on the previous information $x_{n, n-1}$.
         current_measurement (float): current sensor measurement $z_n$.
+        delta (float): track-to-track period in seconds $/Delta t$.
+
 
     Returns:
-        (float): current state estimation based on past and 
-        present information.
+        current_state_estimate_second_derivative (float): current state second
+        derivative estimation based on past and present information 
     """
-    current_state_estimate = current_state_prediction + alpha * (
-        current_measurement - current_state_prediction
+    current_state_estimate_second_derivative = (
+        current_state_prediction_second_derivative + gamma * (
+            (current_measurement - current_state_prediction) /
+            (0.5 * delta**2)
+        )
     )
-    return current_state_estimate 
+    return current_state_estimate_second_derivative
+
 
 # ---------------------------------------------------------------------------- #
 #                                     MAIN                                     #
 # ---------------------------------------------------------------------------- #
-
 if __name__ == "__main__":
-    # Lists of predicted values
+    # List of predicted values
     predicted_positions_list = [
         state_extrapolation_first_equation(
             current_state=INITIAL_ESTIMATED_POSITION,
             delta=DELTA_T,
-            current_state_derivative=INITIAL_ESTIMATED_VELOCITY
+            current_state_derivative=INITIAL_ESTIMATED_VELOCITY,
+            current_state_second_derivative=INITIAL_ESTIMATED_ACCELERATION
         )
     ]
 
     predicted_velocities_list = [
         state_extrapolation_second_equation(
-            current_state_derivative=INITIAL_ESTIMATED_VELOCITY
+            current_state_derivative=INITIAL_ESTIMATED_VELOCITY,
+            current_state_second_derivative=INITIAL_ESTIMATED_ACCELERATION,
+            delta=DELTA_T
+        )
+    ]
+
+    predicted_accelerations_list = [
+        state_extrapolation_third_equation(
+            current_state_second_derivative=INITIAL_ESTIMATED_ACCELERATION
         )
     ]
 
     # Lists of estimated values
     estimated_positions_list = []
     estimated_velocities_list = []
+    estimated_accelerations_list = []
 
     # Number of iterations
     N_ITERATIONS = 10
 
-    # Algorithm cycle
+    # Alogrithm cycle
     for i in range(N_ITERATIONS):
+        # Obtain current estimations based on predictions, filter and
+        # measurements
 
-        # Obtain current estimations based on predictions, filter and measures
         estimated_position_state = state_update_first_equation(
             current_state_prediction=predicted_positions_list[-1],
             alpha=ALPHA,
             current_measurement=MEASUREMENTS[i]
         )
-
+        
         estimated_velocity_state = state_update_second_equation(
             current_state_prediction_derivative=predicted_velocities_list[-1],
             beta=BETA,
@@ -224,31 +295,51 @@ if __name__ == "__main__":
             delta=DELTA_T
         )
 
+        estimated_acceleration_state = state_update_third_equation(
+            current_state_prediction_second_derivative=predicted_accelerations_list[-1],
+            gamma=GAMMA,
+            current_state_prediction=predicted_positions_list[-1],
+            current_measurement=MEASUREMENTS[i],
+            delta=DELTA_T
+        )
+
+
         estimated_positions_list.append(estimated_position_state)
         estimated_velocities_list.append(estimated_velocity_state)
+        estimated_accelerations_list.append(estimated_acceleration_state)
 
         # Obtain predictions
         predicted_position_state = state_extrapolation_first_equation(
             current_state=estimated_position_state,
             delta=DELTA_T,
-            current_state_derivative=estimated_velocity_state
+            current_state_derivative=estimated_velocity_state,
+            current_state_second_derivative=estimated_acceleration_state
         )
 
         predicted_velocity_state = state_extrapolation_second_equation(
-            current_state_derivative=estimated_velocity_state
+            current_state_derivative=estimated_velocity_state,
+            current_state_second_derivative=estimated_acceleration_state,
+            delta=DELTA_T
+        )
+
+        predicted_acceleration_state = state_extrapolation_third_equation(
+            current_state_second_derivative=estimated_acceleration_state
         )
 
         predicted_positions_list.append(predicted_position_state)
         predicted_velocities_list.append(predicted_velocity_state)
+        predicted_accelerations_list.append(predicted_acceleration_state)
 
-    # Creates data table 
+    # Creates data table
     data = np.column_stack(
         [
             [0, *MEASUREMENTS],
             [0, *estimated_positions_list],
             [0, *estimated_velocities_list],
+            [0, *estimated_accelerations_list],
             predicted_positions_list,
-            predicted_velocities_list
+            predicted_velocities_list,
+            predicted_accelerations_list
         ]
     )
 
@@ -258,17 +349,19 @@ if __name__ == "__main__":
             "Measurements",
             "Estimated Position",
             "Estimated Velocity",
+            "Estimated Acceleration",
             "Predicted Position",
-            "Predicted Velocity"
+            "Predicted Velocity",
+            "Predicted Acceleration"
         ]
     )
     data_pd.rename_axis("Iterations", inplace=True)
 
-    data_pd.to_csv("data.csv")  # Saves information
-    print(data_pd)  # Shows pandas DataFrame
+    data_pd.to_csv("data.csv")
+    print(data_pd)
 
     # Plots
-    time = [DELTA_T * i for i in range(len(MEASUREMENTS))]  # Each measurement was made each DELTA_T seconds
+    time = [DELTA_T * i for i in range(len(MEASUREMENTS))]  # x-axis
 
     plt.figure(figsize=(15,10))
     sns.lineplot(
@@ -293,8 +386,50 @@ if __name__ == "__main__":
         label="Prediction"
     )
 
-    plt.title(f"Position vs. Time ($\\alpha =$ {ALPHA}, $\\beta =$ {BETA})")
+    plt.title(f"Position vs. Time ($\\alpha =$ {ALPHA}, $\\beta =$ {BETA}, $\\gamma =$ {GAMMA})")
     plt.xlabel("Time (s)")
     plt.ylabel("Position (m)")
+    plt.savefig("Result_analysis_position.png", dpi=300)
 
-    plt.savefig("Results_analysis.png", dpi=300)
+
+    plt.figure(figsize=(15,10))
+    sns.lineplot(
+        x=time,
+        y=data_pd["Estimated Velocity"][1:],
+        c="red",
+        marker="o",
+        label="Estimates"
+    )
+    sns.lineplot(
+        x=time,
+        y=data_pd["Predicted Velocity"][:-1],
+        c="gray",
+        marker="^",
+        label="Prediction"
+    )
+
+    plt.title(f"Velocity vs. Time ($\\alpha =$ {ALPHA}, $\\beta =$ {BETA}, $\\gamma =$ {GAMMA})")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Velocity (m/2)")
+    plt.savefig("Result_analysis_velocity.png", dpi=300)
+
+    plt.figure(figsize=(15,10))
+    sns.lineplot(
+        x=time,
+        y=data_pd["Estimated Acceleration"][1:],
+        c="red",
+        marker="o",
+        label="Estimates"
+    )
+    sns.lineplot(
+        x=time,
+        y=data_pd["Predicted Acceleration"][:-1],
+        c="gray",
+        marker="^",
+        label="Prediction"
+    )
+
+    plt.title(f"Acceleration vs. Time ($\\alpha =$ {ALPHA}, $\\beta =$ {BETA}, $\\gamma =$ {GAMMA})")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Acceleration ($m/s^2$)")
+    plt.savefig("Result_analysis_acceleration.png", dpi=300)
